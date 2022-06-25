fb77e2a61fb2e200d717ae4b5991c87b3fb4184b
kirankardile <kardilekiran0952@gmail.com>
2022-06-25 10:23:58 +0530

    Commited by KK for High Priority

--- Demo.java
+++ Demo.java
@@ -1,14 +1,25 @@
 public class Demo
 {
+
 	public void m2()
 	{
 		//Logic for bug fix priority
 	}
 
+<<<<<<< Updated upstream
 //Start by Kiran K High Priority
 public void m4()
 {
 //Logicfor High priority method
 }
 //End  by Kiran K High Priority
+=======
+//Start by Kiran K Low Priority
+public void m3()
+{
+//Logicfor Low priority method
+}
+//End  by Kiran K Low Priority
+
+>>>>>>> Stashed changes
 }
